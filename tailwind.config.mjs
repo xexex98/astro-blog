72222b1e7ab3b46ac9f33e530f674db8381bf10b
xexex98 <xexex98@gmail.com>
2024-03-24 00:47:03 +0300

    tailwind staff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'rgb(var(--background) / <alpha-value>)',
+        foreground: 'rgb(var(--foreground) / <alpha-value>)',
+        primary: {
+          DEFAULT: 'rgb(var(--primary) / <alpha-value>)',
+					foreground: 'rgb(var(--primary-foreground) / <alpha-value>)'
+        }
+      }
+    }
+    
   },
   plugins: [require("@tailwindcss/typography")],
 };
