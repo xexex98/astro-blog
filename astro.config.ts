06c3047f0e97248ba27cf5302dfd3997aaebbe33
xexex98 <xexex98@gmail.com>
2024-03-26 23:20:13 +0300

    add million lint fix styles for nav

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import million from "million/compiler";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321/",
   integrations: [react(), tailwind(), sitemap()],
+  vite: {
+    plugins: [million.vite({ mode: "react", server: true, auto: true })],
+  },
 });
